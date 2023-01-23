5fe8bfb805fdb5c8195d31aa56a3cecabddb3e6e
lollomrt <lorenzomartini.mrt@outlook.it>
2023-01-23 13:13:38 +0100

    Creazione oggetto navigazione in app.main - creazione di app header e ciclo FUNZIONANTE

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,88 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
 export default {
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      navigation: {
+        left_navigation: [
+          {
+            titolo: 'Home',
+            url: '#',
+            submenu: [
+              'Main Home',
+              'Italian Restaurant',
+              'Pizzeria Home',
+              'Pizza light',
+              'Pizza dark',
+              'Fast Food home',
+            ]
+          },
+          {
+            titolo: 'Pages',
+            url: '#',
+            submenu: [
+              'about us',
+              'our services',
+              'our team',
+              'our history',
+              'reservation',
+              'book a table I',
+              'book a table II',
+              'contact us',
+              'get in touch',
+              'coming soon',
+              '404 error page',
+            ]
+          },
+          {
+            titolo: 'Menu',
+            url: '#',
+            submenu: [
+              'list type',
+              'list layouts',
+              'product types',
+              'shop pages',
+            ]
+          }
+        ],
 
+        right_navigation: [
+          {
+            titolo: 'Event',
+            url: '#',
+            submenu: [
+              'event list',
+              'event single',
+            ]
+          },
+          {
+            titolo: 'blog',
+            url: '#',
+            submenu: [
+              'right sidebar',
+              'left sidebar',
+              'without sidebar',
+              'posts type',
+            ]
+          },
+          {
+            titolo: 'landing',
+            url: '#',
+            submenu: null
+          }
+        ]
+      }
+    }
+  }
 }
 </script>
 
 <template>
   <div>
-
+    <AppHeader :navig="navigation" />
   </div>
 </template>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,22 @@
 <script>
 export default {
+    props: {
+        navig: Object
+    },
 
 }
 </script>
 
 <template lang="">
     <div>
-        
+        <ul v-for="(element, index) in navig.left_navigation" :key="index">
+            <li><a :href="element.url">{{ element.titolo }}</a></li>                       
+        </ul>
+    </div>
+    <div>
+        <ul v-for="(element, index) in navig.right_navigation" :key="index">
+            <li><a :href="element.url">{{ element.titolo }}</a></li>                       
+        </ul>
     </div>
 </template>
 
